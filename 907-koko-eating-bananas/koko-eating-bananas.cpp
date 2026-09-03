d1832b077ae7a58ae5eb3d922e617f90d1529632
Abhinav Gautam <abhinavgautam.dev@gmail.com>
2026-09-03 13:15:26 +0530

    Time: 10 ms (39.53%) | Memory: 39.3 MB (5.41%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,33 +1,39 @@
 class Solution {
 public:
 
-    long long helper(vector<int> piles , int h,int speed){
+    long long helper(vector<int> piles , int speed){
         long long count = 0;
-        for(int i =0;i<piles.size();i++){
-            count += piles[i]/speed;
+        for(int i=0; i<piles.size() ; i++){
+            count += piles[i] / speed;  
             if(piles[i] % speed != 0){
-                count++;
-            }
+            count++;
         }
+        }
+        
         return count;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
         int low = 1;
-          int high = *max_element(piles.begin(), piles.end());
-
-        int ans =-1;
+        int high = *max_element(piles.begin(),piles.end());
+        int ans = -1;
 
         while(low<=high){
-            int mid = low + (high-low)/2;
 
-            if(helper(piles,h,mid) > h){
-                low = mid+1;
+            int guess = low + (high-low)/2;
+            long long count = helper(piles ,guess );
+
+            if(count > h){
+                low = guess+1;
             }
             else{
-                ans = mid;
-                high = mid-1;
+                ans = guess;
+                high = guess-1;
             }
+
         }
         return ans;
+
+
+        
     }
 };
