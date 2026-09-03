fc93febc812a97553ce5dae8a042c1d27c7d8b18
Abhinav Gautam <abhinavgautam.dev@gmail.com>
2026-09-03 13:16:47 +0530

    Time: 14 ms (19.62%) | Memory: 39.4 MB (5.41%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
 
-    long long helper(vector<int> piles , int speed){
+    inline long long helper(vector<int> piles , int speed){
         long long count = 0;
         for(int i=0; i<piles.size() ; i++){
             count += piles[i] / speed;  
